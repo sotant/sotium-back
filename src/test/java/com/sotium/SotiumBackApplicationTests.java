39cc859784904025e0c72b7f6e216f9c26400ffe
Daniel Soto <sotodani11@gmail.com>
2025-12-29 15:39:04 +0100

    Initial commit

---
+++ src/test/java/com/sotium/SotiumBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sotium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotiumBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
